4cf7508a521ce43ea0751c977fd89a5b087e2b9e
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-07-14 14:17:35 +0800

    added: obstacle draw

--- z_FlappyBox/index.js
+++ z_FlappyBox/index.js
@@ -14,10 +14,10 @@ const state = {
     ],
     obstacles: [
         // {x1: 20, x2: state.obstacles[0].x1 + 20}
-        {x1: 20, x2: 70}, // 2 blocks = 50px, 1tile means 25x25, x1(20) + 2tile gap(50) = x2 = 70
-        {x1: 30, x2: 80},
-        {x1: 10, x2: 60},
-        {x1: 40, x2: 90},
+        {x: 200}, // 2 blocks = 50px, 1tile means 25x25, x(20) + 2tile gap(50) = x2 = 70
+        {x: 300},
+        {x: 100},
+        {x: 350},
     ],
     running: false,
     score: 0,
@@ -31,5 +31,41 @@ console.log(state.obstacles[0].x2)
 
     function startGame() {
         state.running = true
-        scoreEl.textContent = "Ha"
+        scoreEl.textContent = "Obstacle Passed: " + state.score
     }
+
+    function move() {
+
+    }
+
+    function draw() {
+        // Canvas BG
+        ctx.fillStyle = "#e3e6b3"
+        ctx.fillRect(0,0,canvas.width,canvas.height)
+
+        // Obstacle
+        state.obstacles.forEach((coor, i) => {
+
+            // top obstacle
+            ctx.fillStyle = "#04fd00"
+            ctx.fillRect(
+                // i times 100 (50 for width, 60 for gap) + starting gap
+                (i * 110) + 140,
+                0,
+                40,
+                coor.x,
+            )
+
+            // bottom obstacle
+            ctx.fillStyle = "#c08585"
+            ctx.fillRect(
+                (i * 110) + 140,
+                // the + 90 is the gap for bird
+                coor.x + 90,
+                40,
+                canvas.height,
+            )
+        })
+    }
+
+    draw()
